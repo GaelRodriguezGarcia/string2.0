07d00931db9a7f9a7d8030b859b4cc617a3ea204
GaelRodriguezGarcia <53586813+GaelRodriguezGarcia@users.noreply.github.com>
2020-01-23 18:25:00 -0700

    Add files via upload

---
+++ string.js
@@ -0,0 +1,36 @@
+"use strict"
+var doc0 = document.getElementById("one");
+var doc1 = document.getElementById("first");
+var doc2 = document.getElementById("second");
+var doc3 = document.getElementById("third");
+var doc4 = document.getElementById("fifth");
+var doc5 = document.getElementById("fifht")
+var doc6 = document.getElementById("sixth");
+
+
+
+
+
+
+
+
+
+
+
+
+
+function largestNumber(){
+    var largestNum = 0 < 1 < 2 < 3 < 4 < 5 < 6 < 7 < 8 < 9 === true;
+    return = largestNumber;
+}
+
+
+function smallestNumber(){
+    var smallestNum = 0 > 1 > 2 > 3 > 4 > 5 > 6 > 7 > 8 > 9 === true;
+    return = smallestNumber;
+}
+
+
+function mean(){
+
+}
